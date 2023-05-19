0e67ea284bc509ed2c7322cd7a574a5ddb358aa9
Alicodedev <107947617+Alicodedev@users.noreply.github.com>
2023-05-19 01:27:02 -0400

    vote function complete

--- week 3/plurality.c
+++ week 3/plurality.c
@@ -66,6 +66,21 @@ int main(int argc, string argv[])
 // Update vote totals given a new vote
 bool vote(string name)
 {
+    int max_vote = candidates[0].votes;
+
+    for (int i = 0; i < candidate_count; i++){
+        if (max_vote < candidates[i].votes){
+            max_vote = candidates[i].votes;
+        }
+
+    }
+
+    for (int i = 0; i < candidate_count; i++){
+        if(candidates[i].votes == max_vote){
+            printf("%s\n",candidates[i].name);
+        }
+
+    }
 
 
 
