6199835002ca38df102f6d2f41883265d4293206
Alicodedev <107947617+Alicodedev@users.noreply.github.com>
2023-05-22 16:03:38 -0400

    print_winner function complete

--- week 3/runoff.c
+++ week 3/runoff.c
@@ -158,7 +158,14 @@ void tabulate(void)
 
 // Print the winner of the election, if there is one
 bool print_winner(void)
-{
+{   
+    for (int i = 0; i < candidate_count; i++){
+        if (candidates[i].votes > (voter_count/2)){
+            printf("%s\n",candidates[i].name);
+            return true;
+        }
+
+    }
     // TODO
     return false;
 }
