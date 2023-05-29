5d4c7db633e09d6d32c9674458f286a401ba65e9
Alicodedev <107947617+Alicodedev@users.noreply.github.com>
2023-05-29 00:19:59 -0400

    sepia filter function complete

--- week 4/filterless.c
+++ week 4/filterless.c
@@ -22,6 +22,24 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int row = 0; row < width; row++) // loops through each row of the pixels
+    {
+        for(int col = 0; col < height; col++)// loops through each column of the pixels
+        {
+            int sepiaRed, sepiaGreen, sepiaBlue; // each sepiaRGB variable for storing the correct color mix
+
+            sepiaRed = (.393 * image[col][row].rgbtRed) + (.769 * image[col][row].rgbtGreen) + (.189 * image[col][row].rgbtBlue);
+            sepiaGreen = (.349 * image[col][row].rgbtRed) + (.686 * image[col][row].rgbtGreen) + (.168 * image[col][row].rgbtBlue);
+            sepiaBlue = (.272 * image[col][row].rgbtRed) + (.534 * image[col][row].rgbtGreen) + (.131 * image[col][row].rgbtBlue);
+
+            if( sepiaRed <= 255 && sepiaBlue <= 255 && sepiaGreen <= 255){ // keep pixels within color range of 0 255
+
+                image[col][row].rgbtRed = sepiaRed;
+                image[col][row].rgbtBlue = sepiaBlue;
+                image[col][row].rgbtGreen = sepiaGreen;
+                }
+        }
+    }
     return;
 }
 
