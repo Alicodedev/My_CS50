ffb1ec29e70a9cd3b54cd4ef3ee2ee51864edbaa
Alicodedev <107947617+Alicodedev@users.noreply.github.com>
2023-06-09 01:04:43 -0400

    load function complete

--- week 5 /speller.c
+++ week 5 /speller.c
@@ -2,7 +2,9 @@
 
 #include <ctype.h>
 #include <stdbool.h>
-
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -18,6 +20,7 @@ const unsigned int N = 26;
 
 // Hash table
 node *table[N];
+unsigned int word_count , high_value;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
@@ -35,9 +38,36 @@ unsigned int hash(const char *word)
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
-{
+{   
+    FILE *file = fopen(dictionary, "r");
+
+    if (file == NULL){
+        printf("file unable to opens%s\n", dictionary);
+        return false;
+    }
+
+    char word[LENGTH + 1];
+    while(file != NULL)
+    { // reads whole file till the end
+
+        fscanf(file, "*s", word); // reads each character in 
+        node *n = malloc(sizeof(node));
+
+         if ( n == NULL)
+         {
+            return false;
+         }
+
+        strcpy(n->word,word);
+        hash_value = hash(word);// hashing the value of each word
+        n->next = table[hash_value];
+        table[hash_value] = n;
+        word_count++;
+    }
+    fclose(file);
+
     // TODO
-    return false;
+    return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
